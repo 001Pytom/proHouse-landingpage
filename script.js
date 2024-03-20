3b37b1b737e264a4368855126fb8e7e7b36fc414
faatihat <tomfaah@gmail.com>
2024-03-20 12:46:00 +0100

    mobile adjustment

--- script.js
+++ script.js
@@ -19,4 +19,17 @@ leftArrow.addEventListener("click", () => {
   });
 });
 
+//
+const menu = document.querySelector(".ham");
+menu.addEventListener("click", () => {
+  document.querySelector(".mobile-header").style.animation =
+    "slideIn 0.5s forwards";
+  document.querySelector(".mobile-header").style.display = "flex";
+});
 
+const close = document.querySelector(".fa-close");
+close.addEventListener("click", () => {
+  document.querySelector(".mobile-header").style.animation =
+    "slideout 0.5s forwards";
+  document.querySelector(".mobile-header").style.display = "none";
+});
